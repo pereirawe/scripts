5ed258178c743e5df7b46f223d0d6d5f4c6b8e94
William Pereira <pereirawe@gmail.com>
2020-05-04 12:08:49 -0300

    Update termux-youtube-downloader.sh

--- termux-youtube-downloader.sh
+++ termux-youtube-downloader.sh
@@ -1,10 +1,12 @@
 apt update && apt upgrade -y
+
 termux-setup-storage
 apt install python -y
+pip install --updrade pip
 pip install youtube-dl
 mkdir /data/data/com.termux/files/home/storage/shared/Youtube
 mkdir -p ~/.config/youtube-dl
-nano '--no-mtime -o /data/data/com.termux/files/home/storage/shared/Youtube/%(title)s.%(ext)s -f "best[height<=1080]"' >> ~/.config/youtube-dl/config
+echo '--no-mtime -o /data/data/com.termux/files/home/storage/shared/Youtube/%(title)s.%(ext)s -f "best[height<=1080]"' >> ~/.config/youtube-dl/config
 mkdir ~/bin
 cd ~/bin
 echo 'youtube-dl $1' >> termux-url-opener
