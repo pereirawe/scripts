b091c3461995ddb0cfa0d267dc83655acc878487
William Pereira <pereirawe@gmail.com>
2020-05-23 08:08:25 -0300

    Update termux-downloader-installer.sh

--- termux-media-downloader/termux-downloader-installer.sh
+++ termux-media-downloader/termux-downloader-installer.sh
@@ -1,41 +1,44 @@
 #!/bin/bash
 
-# ACTUALIZAR TODO
-apt update && apt upgrade -y
-apt --fix-broken install
-apt autoremove
+# LOCATION URL
+# https://raw.githubusercontent.com/pereirawe/scripts/master/termux-media-downloader/termux-url-opener
 
-# ACTIVAR EL ALMACENAMIENTO DE TERMUX
-termux-setup-storage
+clear
+bold=$(tput bold)
+normal=$(tput sgr0)
 
-# INSTALAR PYTHON, PIP Y LIBRERIAS
-apt install python -y
-pip install --updrade pip
-pip install youtube-dl
-pip install instaloader
+echo "${bold}INSTALANDO TERMUX MEDIA DOWNLOADER${normal}"
+echo $'\nby @pereirawe'
+echo "> ${1}"
 
-# DIRECTORIOS DE ALMACENAMIENTO
-mkdir ~/bin
-mkdir /data/data/com.termux/files/home/storage/shared/Youtube/Movies
-mkdir /data/data/com.termux/files/home/storage/shared/Youtube/Music
-mkdir /data/data/com.termux/files/home/storage/shared/Instagram
+echo $'\n Instalar desde cero? (Y)'
+read -p "> " -n 1 -r
+echo $'\n'
+
+if [[ $REPLY =~ ^[Yy]$ ]]; then
+    # ACTUALIZAR TODO
+    apt update && apt upgrade -y
+    apt --fix-broken install
+    apt autoremove
+
+    # ACTIVAR EL ALMACENAMIENTO DE TERMUX
+    termux-setup-storage
+    # INSTALAR PYTHON, PIP Y LIBRERIAS
+    apt install python -y
+    pip install --updrade pip
+    pip install youtube-dl
+    pip install instaloader
+
+    # DIRECTORIOS DE ALMACENAMIENTO
+    mkdir ~/bin
+    mkdir /data/data/com.termux/files/home/storage/shared/Youtube/Movies
+    mkdir /data/data/com.termux/files/home/storage/shared/Youtube/Music
+    mkdir /data/data/com.termux/files/home/storage/shared/Instagram
+
+fi
 
 cd ~/bin
-# wget https://gist.githubusercontent.com/pereirawe/8bdd1205e6e2481e3e6d8259d51134ed/raw/4b8cc1d26fc0c87c3184f3cac29ca818101ef6e8/termux-url-opener
+rm -f termux-url-opener
 wget https://raw.githubusercontent.com/pereirawe/scripts/master/termux-media-downloader/termux-url-opener
-
-# mkdir /data/data/com.termux/files/home/storage/shared/Twitter
-
-# #CONFIGURACION PARA YOUTUBE
-# mkdir -p ~/.config/youtube-dl
-# echo '--no-mtime -o /data/data/com.termux/files/home/storage/shared/Youtube/%(title)s.%(ext)s -f "best[height<=1080]"' >> ~/.config/youtube-dl/config
-
-# #CONFIGURACION PARA INSTAGRAM
-# mkdir -p ~/.config/instaloader
-# echo '--no-mtime -o /data/data/com.termux/files/home/storage/shared/Instagram/%(title)s.%(ext)s -f "best[height<=1080]"' >> ~/.config/instaloader/config
-
-#CONFIGURACION PARA TWITTER
-# mkdir -p ~/.config/youtube-dl
-# echo '--no-mtime -o /data/data/com.termux/files/home/storage/shared/Twitter/%(title)s.%(ext)s -f "best[height<=1080]"' >> ~/.config/youtube-dl/config
 
 exit
