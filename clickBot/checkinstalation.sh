1d71e8abb2f8d102ec470ecf4b190933002edb3e
William Pereira <pereirawe@gmail.com>
2020-06-18 17:09:13 -0300

    check instalation

---
+++ clickBot/checkinstalation.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+function checkPkg {
+    if [ -z "$1" ]; then
+        echo "var is blank";
+        exit
+    else
+        REQUIRED_PKG=$1
+        PKG_OK=$(dpkg-query -W --showformat='${Status}\n' $REQUIRED_PKG|grep "install ok installed")
+        echo Checking for $REQUIRED_PKG: $PKG_OK
+        if [ "" = "$PKG_OK" ]; then
+        echo "No $REQUIRED_PKG. Setting up $REQUIRED_PKG."
+        sudo apt-get --yes install $REQUIRED_PKG 
+        fi
+    fi
+}
+checkPkg $1
