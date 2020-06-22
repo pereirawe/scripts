4a207f9ea255ef058e6c7a49f5325107d92e4f4e
William Pereira <pereirawe@gmail.com>
2020-06-21 22:30:01 -0300

    WhaLinks new

---
+++ WhaLinks/WhaLinks.php
@@ -0,0 +1,24 @@
+<?php
+
+class WhaLinks {
+    public $arContacts;
+    public $strMessage;
+    public function encodeMessage($name, $phone){
+        $text = 'Oi irmão '.$name.'! Sou o Presidente William Pereira do Ramo Borda do Campo, Da Igreja de Jesus Cristo dos Santos dos Últimos Dias.';
+        $text = str_replace( ' ', '%20' , $text  );
+        $url = 'https://web.whatsapp.com/send?phone=55'.$phone.'&text='.$text;
+        return '<li><a href="'.$url.'" target="new">'.$name.'</a></li>';
+    }
+    public function createLinks($arContacts) {
+        foreach ($arContacts as $name => $phone) {
+            echo $this->encodeMessage($name, $phone);
+        }
+    }
+}
+
+$arContacts = array(
+    'William' => '41987603488',
+    'Liseth' => '41987603487',
+    'Jared' => '41987603486'
+);
+    $WhaLink = new WhaLinks;
