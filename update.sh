da7f8842a64bd5a5fab6a09860ed2ec84c019de6
William Pereira <pereirawe@gmail.com>
2020-10-07 12:01:06 -0300

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,13 +1,11 @@
-#!/bin/bash  
-sudo apt update
+#!/bin/bash
+
+sudo apt-get update -y;
 clear
-sudo apt upgrade
+sudo apt-get upgrade -y
 clear
-sudo apt-get --fix-broken install
+sudo apt-get --fix-broken install -y
 clear
-sudo apt-get autoremove
+sudo apt-get autoremove -y
 clear
 exit
-exit
-
-
